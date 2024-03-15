4ab04256ecf5db17d1ec86b27db8688853b5605a
GlifeTyson <tyson.quach@glife.com.sg>
2024-03-15 11:14:31 +0700

    finish CRUD diary

--- src/controllers/diaryController.js
+++ src/controllers/diaryController.js
@@ -1,3 +1,6 @@
+const { date } = require("joi");
+const { ObjectId } = require("mongodb");
+const mongodb = require("mongodb");
 const diaryController = {
   list: async (req, res) => {
     try {
@@ -14,19 +17,98 @@ const diaryController = {
     }
   },
   view: async (req, res) => {
-    const { mongo } = req.context || {};
-    const { id } = req.params;
-    const diary = mongo.Diary.find({ creator: id });
-    const diaries = await diary.toArray();
-    return res.json(diaries).status(200);
+    try {
+      const { mongo } = req.context || {};
+      const { id } = req.params;
+      const diary = mongo.Diary.find({ creator: id });
+      const diaries = await diary.toArray();
+      return res.json(diaries).status(200);
+    } catch (error) {
+      return res.json({ message: error.message }).status(422);
+    }
   },
   create: async (req, res) => {
-    // const { mongo } = req.context || {};
-    // const { title, content, creator } = req.body;
-    // const diary = mongo.Diary.insertOne({title:title,content:content,creator:creator,viewer:"",createdAt:Date.now()});
+    try {
+      const { mongo } = req.context || {};
+      const { title, content, creator } = req.body;
+      if (title.length == 0 || content.length == 0) {
+        return res
+          .json({ message: "Please fill in title and content" })
+          .status(422);
+      }
+      const diary = mongo.Diary.insertOne({
+        title: title,
+        content: content,
+        creator: creator,
+        viewer: "",
+        createdAt: Date.now(),
+        deleteAt: "",
+        updateAt: "",
+      });
+      return res.json({ message: "Created successfully" }).status(200);
+    } catch (error) {
+      return res.json({ message: error.message }).status(422);
+    }
   },
-  update: async (req, res) => {},
-  delete: async (req, res) => {},
+  update: async (req, res) => {
+    try {
+      const { mongo } = req.context || {};
+      const { title, content } = req.body;
+      const { id } = req.params;
+      if (title.length == 0 || content.length == 0) {
+        return res
+          .json({ message: "Please fill in title and content" })
+          .status(422);
+      }
+      //parse id bc find query _id need ObjectID type to compare not String
+      const parseId = new mongodb.ObjectId(id);
+      const diariesFound = mongo.Diary.find({ _id: parseId });
+      const diary = await diariesFound.toArray();
+      if (diary.length == 0) {
+        return res
+          .json({ message: "The diary not found to update" })
+          .status(422);
+      }
+      //   return res.json(diary).status(200);
+      await mongo.Diary.updateOne(
+        { _id: parseId },
+        {
+          $set: { title: title, content: content, updateAt: Date.now() },
+        }
+      );
+      return res.json({ message: "Updated successfully" }).status(200);
+    } catch (error) {
+      return res.json({ message: error.message }).status(422);
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const { mongo } = req.context || {};
+      const { id } = req.params;
+      //parse id bc find query _id need ObjectID type to compare not String
+      const parseId = new mongodb.ObjectId(id);
+      const diariesFound = mongo.Diary.find({ _id: parseId });
+      const diary = await diariesFound.toArray();
+      if (diary.length == 0) {
+        res
+          .json({
+            message: "Not found to delete",
+          })
+          .status(422);
+      }
+
+      await mongo.Diary.updateOne(
+        { _id: parseId },
+        {
+          $set: { deleteAt: Date.now() },
+        }
+      );
+      //   //   await mongo.Diary.findOneAndDelete({ _id: parseId });
+      res.json({ message: "Soft Deleted successfully" }).status(200);
+    } catch (error) {
+      return res.json({ message: error.message }).status(422);
+    }
+  },
 };
 
 module.exports = diaryController;
--- src/routes/diaryRoute.js
+++ src/routes/diaryRoute.js
@@ -4,9 +4,9 @@ const diaryController = require("../controllers/diaryController");
 const diaryRoute = Router();
 
 diaryRoute.get("/api/diaries", diaryController.list); //get all diaries
-diaryRoute.get("/api/diaries/:id", diaryController.view); //search diary by user
-diaryRoute.post("/api/diaries", diaryController.create);
+diaryRoute.get("/api/diaries/:id", diaryController.view); //get diary by user
+diaryRoute.post("/api/diaries", diaryController.create); //create new diary(note)
 diaryRoute.patch("/api/diaries/:id", diaryController.update);
-diaryRoute.delete("/api/diaries/:id", diaryController.delete);
+diaryRoute.delete("/api/diaries/:id", diaryController.delete); //soft delete
 
 module.exports = diaryRoute;
